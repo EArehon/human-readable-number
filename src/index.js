0e20d87f3bed795b8154e9d7bb56e34234bddc6b
EArehon <32142446+EArehon@users.noreply.github.com>
2020-03-03 11:09:56 +0200

    feat: add toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
-  
+	let a = ["zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
+  let b = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+  let res = '';
+
+	if(number < 20){
+  	res = a[number];
+  }
+  else if(number >= 20 && number <100){
+  	res = number%10 == 0 ? b[Math.floor(number/10) - 2] : b[Math.floor(number/10) - 2] + " " + a[number%10];
+  }
+  else if(number >= 100 && number <1000){
+  	if (number%100 == 0){
+    	res = a[Math.floor(number/100)] + " hundred";
+    }
+  	else{
+    let tempHund = Math.floor(number/100);
+    let newNumber = number -  tempHund*100;
+    
+    res = a[tempHund] + " hundred " +  toReadable(newNumber);
+    }
+  }
+
+return res;
 }
